7442435ca391e41fdf15b57f05edee73a19f1137
merinors <merinors@192.168.0.12>
2017-03-25 19:04:32 +0100

    lkwenf

--- Hundir/src/code/Tablero.java
+++ Hundir/src/code/Tablero.java
@@ -1,5 +1,35 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Tablero {
 
+	private ArrayList<ArrayList<Integer>> tablero;
+	private int ancho;
+	private int alto;
+	
+	public Tablero(){
+		ancho=10;
+		alto=10;
+	}
+	
+	
+	public void inicializar(){
+	
+		int a=10;
+		ArrayList<Integer> lis;
+		Iterator<Integer> iter;
+		Iterator<ArrayList<Integer>> it=tablero.iterator();
+		while(it.hasNext()){
+			lis=it.next();
+			for (int i=0;i<ancho;i++){
+				lis.add(a);
+				a--;
+			}
+			a=10;			
+			
+		}
+		
+	}
 }
